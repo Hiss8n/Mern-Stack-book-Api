d16411cbe2191f65e75e15a1e82acd2c0c9b904a
Hissen Tutu <126790912+Hiss8n@users.noreply.github.com>
2024-08-28 10:32:54 +0300

    Update db.js

--- server/DataBase/db.js
+++ server/DataBase/db.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 const mongoose=require('mongoose')
 const MONG0_DB=process.env.MONG0_DB
 
-mongoose.connect('mongodb+srv://test123:test123@cluster0.awvv3s7.mongodb.net/books?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
+mongoose.connect('MONGO_DB').then(()=>{
     console.log('Databae connected succeessfully!!')
 }).catch((e)=>console.log(e))
